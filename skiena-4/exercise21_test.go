018ef5a80f3bb29e3a962904636ab2c8454db9be
Michael Long <mikejlong60@gmail.com>
2025-04-08 09:14:24 -0400

    exercise 21

--- skiena-4/exercise21_test.go
+++ skiena-4/exercise21_test.go
@@ -1,37 +1,76 @@
 package skiena_4
 
 import (
-	"math"
+	"math/rand"
 	"testing"
 )
 
 //  Find the median of a set of numbers using some sort of partitioning technique like Quicksort.
+/** Algorithm
+1. Pick a pivot point in the array. Can be anything
+2. Iterate through the array, swapping the pivot value until you have placed it in the correct index of the array.
+   You can have more than 1 value, the array is not a set. This is the array for this example: {5, 2, 5, 1, 5, 3, 9, 5}.
+   Your initial pivot is index 0, the value 5.  But the choice of pivot point does not matter.
+3. Once you have done that you end up with three sub-arrays: the one which contains the pivot value, in this case all the 5s,
+   the one to the left which contains everything less than the pivot value, and the one to the right which contains
+   everything greater than the pivot value.
+*/
 
-func TestBigOhNMedian(t *testing.T) {
-	bigOhMedian := func(mid int, xs []int) int {
-		start := xs[mid]
-		swap := func(x int, y int, xs []int) {
-			tx := xs[x]
-			xs[x] = y
-			xs[y] = tx
-		}
+func partition(xs []int, left, right int) int {
+	pivotIndex := rand.Intn(right-left+1) + left
+	//NOTE - This a special double assignment statement.
+	//It evaluates 2 right side variables first and then assigns to left
+	//side without the need for a temporary variable like I would need in C.
+	xs[pivotIndex], xs[right] = xs[right], xs[pivotIndex]
+	pivot := xs[right]
+	storeIndex := left
 
-		for i := 0; i < mid; i++ {
-			if start < xs[i] {
-				swap(start, xs[i], xs)
-			}
+	for i := left; i < right; i++ {
+		//This does the swap
+		if xs[i] < pivot {
+			//Double assignment statement.
+			xs[storeIndex], xs[i] = xs[i], xs[storeIndex]
+			storeIndex++
 		}
-		return xs[0]
+	}
+	//Double assignment statement.
+	xs[right], xs[storeIndex] = xs[storeIndex], xs[right]
+	return storeIndex
+}
+
+func quickSelect(xs []int, left, right, k int) int {
+	if left == right {
+		return xs[left]
 	}
 
-	xs := []int{0, 2, 7, 6, 11, 10, 5, 8, 9}
+	pivotIndex := partition(xs, left, right)
+	if k == pivotIndex {
+		return xs[k]
+	} else if k < pivotIndex {
+		return quickSelect(xs, left, pivotIndex-1, k)
+	} else {
+		return quickSelect(xs, pivotIndex+1, right, k)
+	}
+}
 
-	m := float64(len(xs) / 2)
-	mid :=int(math.Ceil(m))
+func TestQuickSelect(t *testing.T) {
 
-	var actual = bigOhMedian(mid, xs)
+	var actual int
+	xs := []int{7, 2, 1, 6, 8, 5, 3, 4}
 
-	expected := 8
+	n := len(xs)
+	k := n / 2
+	if n%2 == 1 {
+		//odd number of elements
+		actual = quickSelect(xs, 0, n-1, k)
+	} else {
+		//even number of elements
+		left := quickSelect(xs, 0, n-1, k-1)
+		right := quickSelect(xs, 0, n-1, k)
+		actual = (left + right) / 2
+	}
+
+	expected := 4
 	if expected != actual {
 		t.Errorf("Expected:[%v], Actual:[%v]", expected, actual)
 	}
