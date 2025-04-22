1d92dbb7718b1da618b2e966bd0c07b99ff29c83
mikejlong60 <mikejlong60@gmail.com>
2025-04-22 10:05:55 -0400

    asafqdV

--- skiena-4/exercise23_test.go
+++ skiena-4/exercise23_test.go
@@ -27,31 +27,45 @@ Start at the first element.
   done
 */
 
-func orderAsRedWhiteAndBlue(xs []string) {
-	var i = 0
-	var j = i + 1
-	var currentColor = "r"
-	//var nextColor = "w"
+var steps = 0
 
+func orderAsRedWhiteAndBlue(xs []string, i int, currentColor string) int {
+	var j = 0
 	for ; i < len(xs); i++ {
-		j = i
+		if j == len(xs) { //never reached
+			return i
+		}
+		steps++
+		//j := i + 1
 		if xs[i] == currentColor {
 			continue
 		} else {
+			//matchedCurrentColor := false
+			j = i
 			for ; j < len(xs); j++ {
+				steps++
 				if xs[j] == currentColor {
+					//matchedCurrentColor = true
 					xs[i], xs[j] = xs[j], xs[i]
-					break
+					if j < len(xs) {
+						break
+					}
 				}
 			}
+			//if matchedCurrentColor {
+			//	return i
+			//}
 		}
 	}
+	return i
 }
 
 func TestOrderAsRedWhiteAndBlue(t *testing.T) {
-	xs := []string{"r", "r", "b", "r", "w", "b", "r", "w", "r"}
+	xs := []string{"r", "r", "b", "r", "w", "b", "r", "r", "b", "r", "w", "b"}
 
-	orderAsRedWhiteAndBlue(xs)
+	i := orderAsRedWhiteAndBlue(xs, 0, "r")
+	orderAsRedWhiteAndBlue(xs, i+1, "w")
+	fmt.Println(steps)
 	fmt.Println(xs)
 	//expected := []string{"r","r","r","r","w","w","b","b"}
 
