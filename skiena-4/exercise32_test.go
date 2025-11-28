576371ec1f744b5f6b86026cc010c858b1f789d1
mikejlong60 <mikejlong60@gmail.com>
2025-11-28 09:48:13 -0500

    sadfdgf

--- skiena-4/exercise32_test.go
+++ skiena-4/exercise32_test.go
@@ -2,6 +2,7 @@ package skiena_4
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 
@@ -16,34 +17,51 @@ import (
 // Do it in O(n) in space O(1)
 //Possible answers for a == [3,1,4,2,6,5] are [1,3,2,5,4,6]
 
+//Possible answers for a == [3,1,4,2,6,5, 7, 8] are [1,3,2,5,4,7,6,8]
+
 //1,3,2,4,6,5
 
 func wiggleSort(xs []int) []int {
-	for i := 0; i < len(xs); i++ {
-		if i < len(xs)-2 && xs[i] > xs[i+1] && xs[i] < xs[i+2] { //1,3,2,4 case
+	for i := 0; i < len(xs); {
+		if i+2 < len(xs) && xs[i] > xs[i+1] {
 			xs[i], xs[i+1] = xs[i+1], xs[i]
-			continue
 		}
-		if i < len(xs)-3 && xs[i] > xs[i+1] && xs[i] > xs[i+2] { //4,2,6,5 case
-			//swap 2[i+1],4[i] then you have 2,4,6,5, then swap 4[i+1] and 5[i+3]
-			xs[i+1], xs[i] = xs[i], xs[i+1]
-			xs[i+1], xs[i+3] = xs[i+3], xs[i+1]
-			continue
-		}
-		if i < len(xs)-1 && xs[i] > xs[i+1] { // now you can fall through
-			xs[i], xs[i+1] = xs[i+1], xs[i]
-			continue
-		}
+		i = i + 2
 	}
+
+	for i := 1; i < len(xs); {
+		if i+2 < len(xs) && xs[i] < xs[i+1] {
+			xs[i], xs[i+1] = xs[i+1], xs[i]
+		}
+		i = i + 2
+	}
+	//if !(xs[0] < xs[1] && xs[2] < xs[1] && xs[2] < xs[3]) {
+	//	xs[i], xs[3] = xs[3], xs[i]
+	//}
+	//if i < len(xs)-2 && xs[i] > xs[i+1] && xs[i] < xs[i+2] { //1,3,2,4 case
+	//	xs[i], xs[i+1] = xs[i+1], xs[i]
+	//	continue
+	//}
+	//if i < len(xs)-3 && xs[i] > xs[i+1] && xs[i] > xs[i+2] { //4,2,6,5 case
+	//	//swap 2[i+1],4[i] then you have 2,4,6,5, then swap 4[i+1] and 5[i+3]
+	//	xs[i+1], xs[i] = xs[i], xs[i+1]
+	//	xs[i+1], xs[i+3] = xs[i+3], xs[i+1]
+	//	continue
+	//}
+	//if i < len(xs)-1 && xs[i] > xs[i+1] { // now you can fall through
+	//	xs[i], xs[i+1] = xs[i+1], xs[i]
+	//	continue
+	//}
+	//}
 
 	return xs
 }
 
 func TestWiggleSort2(t *testing.T) {
-	xs := []int{3, 1, 4, 2, 6, 5}
+	xs := []int{3, 1, 4, 2, 6, 5, 7, 8}
 
 	actual := wiggleSort(xs)
-	expected := []int{1, 3, 2, 5, 4, 6}
+	expected := []int{1, 3, 2, 5, 4, 7, 6, 8}
 	eq := func(l, r int) bool {
 		if l == r {
 			return true
@@ -71,36 +89,45 @@ func TestWiggleSort(t *testing.T) {
 		}
 	}
 
-	maxListSize := 3000 //Am banking on the odds being near zero that I get the same bool over 3000 rolls of the die.
-	minListSize := 2
+	maxListSize := 100 //Am banking on the odds being near zero that I get the same bool over 3000 rolls of the die.
+	minListSize := 10
 	ge := propcheck.ChooseInt(0, 20000)
 	ge2 := sets.ChooseSet(minListSize, maxListSize, ge, lt, eq)
 	rng := propcheck.SimpleRNG{Seed: time.Now().Nanosecond()}
-	makeSetEvenNumberOfElements := func(xs []int) []int {
+	runWiggleSort := func(xs []int) []int {
+		var r []int
 		if (len(xs) % 2) != 0 {
-			return xs[1:]
+			r = xs[1:]
+		} else {
+			r = xs
 		}
-		return xs
+		rand.Shuffle(len(r), func(i, j int) {
+			r[i], r[j] = r[j], r[i]
+		})
+		wiggleSort(r)
+		return r
 	}
 
-	evenElementsAscend := propcheck.ForAll(ge2, fmt.Sprintf("List must have at least A length of %v \n", minListSize),
-		makeSetEvenNumberOfElements,
+	evenElementsAscend := propcheck.ForAll(ge2, "even element pairs ascend",
+		runWiggleSort,
 		func(xs []int) (bool, error) {
-			for i := 0; i < len(xs); i++ {
-				if xs[i] < xs[i+1] {
+			for i := 0; i < len(xs); {
+				if i+2 < len(xs) && xs[i] > xs[i+1] {
 					return false, fmt.Errorf("even integers not in ascending sequence")
 				}
+				i = i + 2
 			}
 			return true, nil
 		},
 	)
-	oddElementsAscend := propcheck.ForAll(ge2, fmt.Sprintf("List must have A length less than %v \n", maxListSize),
-		makeSetEvenNumberOfElements,
+	oddElementsAscend := propcheck.ForAll(ge2, "uneven element pairs descend",
+		runWiggleSort,
 		func(xs []int) (bool, error) {
-			for i := 1; i < len(xs); i++ {
-				if xs[i] < xs[i+1] {
-					return false, fmt.Errorf("odd integers not in ascending sequence")
+			for i := 1; i < len(xs); {
+				if i+2 < len(xs) && xs[i] < xs[i+1] {
+					return false, fmt.Errorf("uneven integers not in descending sequence")
 				}
+				i = i + 2
 			}
 			return true, nil
 		},
