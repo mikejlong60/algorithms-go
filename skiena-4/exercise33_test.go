75cc0e37a4a2f3a070e23b9c4dd246a8e4513e50
mikejlong60 <mikejlong60@gmail.com>
2025-12-09 10:27:40 -0500

    Heapsort

--- skiena-4/exercise33_test.go
+++ skiena-4/exercise33_test.go
@@ -1,77 +1,19 @@
 package skiena_4
 
-import (
-	"fmt"
-	"sort"
-	"testing"
-	"time"
+// Show that you can sort an array of k distinct integers in O(n log k) steps, better than O(n log n).  Think of sorting
+// an array of 10,000 ones and zeros.
 
-	"github.com/greymatter-io/golangz/arrays"
-	"github.com/greymatter-io/golangz/propcheck"
-	"github.com/greymatter-io/golangz/sorting"
-	"github.com/mikejlong60/algorithms-go/chapter5"
-	log "github.com/sirupsen/logrus"
-)
+//This can be shown to be true using a heap. Since any value can be inserted into a heap in log n steps where n is the
+//size of the heap.  In the case of the examole above where k is two, the algorithm is as follows:
 
-// Interesting fact about my quicksort versus golang one versus my non-parallel merge sort.  My Quicksort implementation is faster than Golang's
-// as the number of duplicate elements decreases. Golang's sort gets faster the more duplication exists.
-// Merge sort is always about 7x slower than either.
-func TestMergeSortPerformanceVersusYourQuicksortAndGolangQuicksort(t *testing.T) {
-
-	lt := func(l, r int) bool {
-		if l < r {
-			return true
-		} else {
-			return false
-		}
-	}
-	eq := func(l, r int) bool {
-		if l == r {
-			return true
-		} else {
-			return false
-		}
-	}
-	g0 := propcheck.ChooseInt(1, 2)
-	g1 := propcheck.ChooseArray(10000, 10000, g0)
-	rng := propcheck.SimpleRNG{Seed: time.Now().Nanosecond()}
-	prop := propcheck.ForAll(g1,
-		"Test Mergesort  \n",
-		func(xs []int) []int {
-			return xs
-		},
-		func(xs []int) (bool, error) {
-			var errors error
-
-			var start = time.Now()
-			l := len(xs)
-			actual := chapter5.MergeSort(xs, lt)
-			log.Infof("Mergesort array of:%v ints took:%v", l, time.Since(start))
-
-			expected := make([]int, l)
-			copy(expected, xs)
-			start = time.Now()
-			sort.Ints(expected)
-			log.Infof("Golang sort array of:%v ints took:%v", l, time.Since(start))
-
-			start = time.Now()
-			gs2 := make([]int, l)
-			copy(gs2, xs)
-			sorting.QuickSort(gs2, lt)
-			log.Infof("My quicksort array of:%v ints took:%v", l, time.Since(start))
-
-			if !arrays.ArrayEquality(actual, expected, eq) {
-				errors = fmt.Errorf("Actual:%v Expected:%v", actual, expected)
-			}
-
-			if errors != nil {
-				return false, errors
-			} else {
-				return true, nil
-			}
-		},
-	)
-	result := prop.Run(propcheck.RunParms{1, rng})
-	propcheck.ExpectSuccess[[]int](t, result)
-
+/**
+heap y
+for i, _ := range xs { // O(log k) * n total steps.
+	incrementI(y, i)
 }
+
+Now just make a new array by popping off the heap until it is empty and writing number of increment elements for k.
+
+I think this must be how heap sort is implemented.
+
+*/
