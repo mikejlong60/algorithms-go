44e78fb9b73dad497c6b3cc8d8b6e589e7ff6824
Mike Long <mikejlong60@gmail.com>
2026-01-06 09:19:13 -0500

    not right yet

--- skiena-4/exercise37_test.go
+++ skiena-4/exercise37_test.go
@@ -5,7 +5,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/greymatter-io/golangz/arrays"
 	"github.com/greymatter-io/golangz/propcheck"
+	"github.com/greymatter-io/golangz/sorting"
 )
 
 // Given an array of consisting of ones and zeros,
@@ -14,43 +16,42 @@ import (
 
 func TestNMinus1Comparisons(t *testing.T) {
 	rng := propcheck.SimpleRNG{time.Now().Nanosecond()}
-	res := propcheck.ChooseArray(10, 10, propcheck.ChooseInt(0, 2))
-	sortIt := func(xs []int) propcheck.Pair[[]int, int] {
+	res := propcheck.ChooseArray(100000, 1000000, propcheck.ChooseInt(0, 2))
+	sortIt := func(xs []int) []int {
 		fmt.Printf("Generated array of length:%v\n", len(xs))
-		totalSteps := 0
 		i := 0
 		maybeNextZero := 0
 		for i < len(xs)-1 {
-			if xs[i] < xs[i+1] {
-				totalSteps++
-				i++
-				//maybeNextZero++
-			} else if xs[i] > xs[i+1] {
-				xs[i], xs[i+1] = xs[i+1], xs[i]
-				//maybeNextZero = i + 1
-				totalSteps++
-				i++
-			} else if xs[i] == xs[i+1] && xs[i] == 1 {
-				//look for next 0
+			if xs[i] == 1 { //see if there are any zeros past here
 				for j := maybeNextZero; j < len(xs); j++ {
-					totalSteps++
 					if xs[j] == 0 {
-						//swap them
 						xs[i], xs[j] = xs[j], xs[i]
-						maybeNextZero = j
+						maybeNextZero = j + 1
 						break
 					}
+					if j == len(xs)-1 {
+						return xs
+					}
 				}
-				i++
 			} else {
-				i++
-				fmt.Println("error asdfqsgqsgasgqfg")
+				maybeNextZero = i + 1
 			}
+			i++
 		}
-		return propcheck.Pair[[]int, int]{xs, totalSteps}
+		return xs
 	}
-	verifySort := func(p propcheck.Pair[[]int, int]) (bool, error) {
-		fmt.Printf("total steps:%v\n", p.B)
+	verifySort := func(actual []int) (bool, error) {
+		//fmt.Printf("xs:%v\n", actual)
+		lt := func(x, y int) bool {
+			return x < y
+		}
+		expected := make([]int, len(actual))
+		copy(expected, actual)
+		sorting.QuickSort(expected, lt)
+		r := arrays.ArrayEquality(actual, expected, func(l, r int) bool { return l == r })
+		if !r {
+			return false, fmt.Errorf("expected %v, got %v", expected, actual)
+		}
 		return true, nil
 	}
 	test := propcheck.ForAll(res, "Sort an array of ones and zeros in n-1 comparisons.", sortIt, verifySort)
