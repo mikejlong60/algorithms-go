4f98d15149f150ea11a21d244b9d83b172bc15c3
Mike Long <mikejlong60@gmail.com>
2026-03-13 08:49:58 -0400

    Uses option ... vast simplification

--- skiena-4/exercise42_test.go
+++ skiena-4/exercise42_test.go
@@ -3,30 +3,57 @@ package skiena_4
 import (
 	"fmt"
 	"testing"
+
+	"github.com/greymatter-io/golangz/option"
+	"github.com/greymatter-io/golangz/propcheck"
 )
 
-func isRamanujanNumber(firstCubedNumber, bCandidate int, maybeRamanujanNumber int) bool {
-	cubed := func(x int) int {
-		return x * x * x
+var steps2 = 0
+
+func cubed(x int) int {
+	return x * x * x
+}
+
+func ramanujanNumber(aCandidate, bCandidate int, ramanujanPair propcheck.Pair[int, int], maybeRamanujanNumber int) option.Option[propcheck.Pair[int, int]] {
+	steps2++
+	cubedACandidate := cubed(aCandidate)
+	cubedBCandidate := cubed(bCandidate)
+	if cubedACandidate > maybeRamanujanNumber {
+		return option.None[propcheck.Pair[int, int]]{}
+	}
+	if cubedACandidate+cubedBCandidate > maybeRamanujanNumber {
+		return option.None[propcheck.Pair[int, int]]{}
 	}
 
-	if firstCubedNumber > maybeRamanujanNumber {
-		return false
-	}
-	if firstCubedNumber+cubed(bCandidate) > maybeRamanujanNumber {
-		return false
-	}
-
-	if firstCubedNumber+cubed(bCandidate) == maybeRamanujanNumber {
-		return true
+	if cubedACandidate+cubedBCandidate == maybeRamanujanNumber {
+		if bCandidate == ramanujanPair.A || bCandidate == ramanujanPair.B { // You already found this one
+			return ramanujanNumber(aCandidate, bCandidate+1, ramanujanPair, maybeRamanujanNumber)
+		} else {
+			return option.Some[propcheck.Pair[int, int]]{propcheck.Pair[int, int]{aCandidate, bCandidate}}
+		}
 	}
 
-	return isRamanujanNumber(firstCubedNumber, bCandidate+1, maybeRamanujanNumber)
+	return ramanujanNumber(aCandidate, bCandidate+1, ramanujanPair, maybeRamanujanNumber)
+}
+
+func findAPair(maybeRamanujanNumber int, ramanujanPair propcheck.Pair[int, int]) option.Option[propcheck.Pair[int, int]] {
+	for i := 0; i <= maybeRamanujanNumber; i++ {
+		b := ramanujanNumber(i, i, ramanujanPair, maybeRamanujanNumber)
+		switch v := b.(type) {
+		case option.None[propcheck.Pair[int, int]]:
+			b = ramanujanNumber(i, i+1, ramanujanPair, maybeRamanujanNumber)
+		default:
+			return v
+		}
+	}
+	return option.None[propcheck.Pair[int, int]]{}
 }
 
 func TestIsRamanujanNumber(t *testing.T) {
-
-	isRamanujanNumber := isRamanujanNumber(0, 1, 1729)
-
-	fmt.Println(isRamanujanNumber)
+	aa := func(x propcheck.Pair[int, int]) option.Option[propcheck.Pair[int, int]] {
+		return findAPair(1729, x)
+	}
+	ramanujanPair1 := findAPair(1729, propcheck.Pair[int, int]{})
+	ramanujanPair2 := option.FlatMap[propcheck.Pair[int, int]](ramanujanPair1, aa)
+	fmt.Printf("pair1:%v pair2:%v steps:%v\n", ramanujanPair1, ramanujanPair2, steps2)
 }
