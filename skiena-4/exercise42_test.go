2dfabe3f12b31445c65265130c66023b697f335e
Mike Long <mikejlong60@gmail.com>
2026-03-16 16:30:33 -0400

    Improved test

--- skiena-4/exercise42_test.go
+++ skiena-4/exercise42_test.go
@@ -28,9 +28,10 @@ func ramanujanNumber(aCandidate, bCandidate int, ramanujanPair propcheck.Pair[in
 	if cubedACandidate+cubedBCandidate == maybeRamanujanNumber {
 		if bCandidate == ramanujanPair.A || bCandidate == ramanujanPair.B { // You already found this one
 			return ramanujanNumber(aCandidate, bCandidate+1, ramanujanPair, maybeRamanujanNumber)
-		} else {
-			return option.Some[propcheck.Pair[int, int]]{propcheck.Pair[int, int]{aCandidate, bCandidate}}
 		}
+
+		return option.Some[propcheck.Pair[int, int]]{propcheck.Pair[int, int]{aCandidate, bCandidate}}
+
 	}
 
 	return ramanujanNumber(aCandidate, bCandidate+1, ramanujanPair, maybeRamanujanNumber)
@@ -38,7 +39,7 @@ func ramanujanNumber(aCandidate, bCandidate int, ramanujanPair propcheck.Pair[in
 
 func findAPair(maybeRamanujanNumber int, ramanujanPair propcheck.Pair[int, int]) option.Option[propcheck.Pair[int, int]] {
 	for i := 0; i <= maybeRamanujanNumber; i++ {
-		b := ramanujanNumber(i, i, ramanujanPair, maybeRamanujanNumber)
+		b := ramanujanNumber(i, i+1, ramanujanPair, maybeRamanujanNumber)
 		switch v := b.(type) {
 		case option.None[propcheck.Pair[int, int]]:
 			b = ramanujanNumber(i, i+1, ramanujanPair, maybeRamanujanNumber)
@@ -53,7 +54,22 @@ func TestIsRamanujanNumber(t *testing.T) {
 	aa := func(x propcheck.Pair[int, int]) option.Option[propcheck.Pair[int, int]] {
 		return findAPair(1729, x)
 	}
+	verify := func(actual option.Option[propcheck.Pair[int, int]], expected propcheck.Pair[int, int]) {
+		switch v := actual.(type) {
+		case option.None[propcheck.Pair[int, int]]:
+			t.Errorf("unexpected none")
+		case option.Some[propcheck.Pair[int, int]]:
+			if !(v.Value.A == expected.A && v.Value.B == expected.B) {
+				t.Errorf("expected%v but got %v", expected, v)
+			}
+		default:
+			t.Errorf("unexpected failure")
+		}
+
+	}
 	ramanujanPair1 := findAPair(1729, propcheck.Pair[int, int]{})
 	ramanujanPair2 := option.FlatMap[propcheck.Pair[int, int]](ramanujanPair1, aa)
-	fmt.Printf("pair1:%v pair2:%v steps:%v\n", ramanujanPair1, ramanujanPair2, steps2)
+	verify(ramanujanPair1, propcheck.Pair[int, int]{1, 12})
+	verify(ramanujanPair2, propcheck.Pair[int, int]{9, 10})
+	fmt.Printf("steps%v\n", steps2)
 }
