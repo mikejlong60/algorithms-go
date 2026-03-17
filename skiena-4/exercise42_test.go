e81366dcf9f6e8b971d00473de28acebdb9c5810
Mike Long <mikejlong60@gmail.com>
2026-03-17 09:01:02 -0400

    First 20 or so ramanujan numbers

--- skiena-4/exercise42_test.go
+++ skiena-4/exercise42_test.go
@@ -95,7 +95,7 @@ func TestIsRamanujanNumber2(t *testing.T) {
 					t.Errorf("expected{{2, 16, 9, 15}} for %v but got %v and took %v steps", x, v, steps2)
 				}
 			} else {
-				fmt.Printf("another Ramanujan number %v with cube roots%v and steps:%v", x, v, steps2)
+				fmt.Printf("another Ramanujan number %v with cube roots%v and steps:%v\n", x, v, steps2)
 			}
 
 		default:
